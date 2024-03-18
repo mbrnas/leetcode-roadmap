2827053325bdc0bad969663c32fc21763b87f0c1
Matija Brnas <mb8828@rit.edu>
2024-03-18 23:12:12 +0100

    implemented two sum

--- src/main/java/org/company/arraysandhashing/TwoSum.java
+++ src/main/java/org/company/arraysandhashing/TwoSum.java
@@ -1,5 +1,18 @@
 package org.company.arraysandhashing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
-    //TODO implement two sum method
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> num_map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (num_map.containsKey(complement)) {
+                return new int[] {num_map.get(complement), i};
+            }
+            num_map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException();
+    }
 }
